7f20620b3ddd88caa2c020955fbe0b314bcd872f
LeeDongHui <67128895+chdonghui@users.noreply.github.com>
2021-09-14 23:56:54 +0900

    Add files via upload

---
+++ 0422/연습1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	
+	printf("C언어 프로그램을 작성해 봅시다"); 
+	return 0;
+	
+}
---
+++ 0422/연습2.cpp
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+int main(){
+	
+	system("start \magnify\ C:\\windows\\system32\\magnify.exe");
+	return 0;
+	
+}
