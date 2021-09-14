06e9364707c9c9e6b871278d9d8dd9070b6f55eb
LeeDongHui <67128895+chdonghui@users.noreply.github.com>
2021-09-15 00:09:58 +0900

    Delete 0520 directory

--- 0520/day7/2차원 배열_4.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int r = 0;
-	int c = printf("InputNumber:");
-	int d = scanf("%d", &r);
-	printf("printf의 리턴값: %d\nscanf의 리턴값: %d\n", c, d);
-	
-	return 0;
-}
--- 0520/day7/학습활동_3.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	
-	char str[3][20] = {"hello, juior", "software world", "Good!"};
-	printf("%s\n", str[0]);
-	printf("%s\n", str[1]);
-	printf("%s\n", str[2]);
-	return 0;
-}
