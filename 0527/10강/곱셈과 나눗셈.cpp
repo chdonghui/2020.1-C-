d9fca84dccb050234e7813e0557455f14c6a1022
LeeDongHui <67128895+chdonghui@users.noreply.github.com>
2021-09-15 00:32:27 +0900

    Rename 곱셈과 나눗셈.cpp to 0527/10강/곱셈과 나눗셈.cpp

---
+++ 0527/10강/곱셈과 나눗셈.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	
+	int a=2, b=1;
+	
+	printf("%d 더하기 %d = %d\n", a, b, a+b);
+	printf("%d 빼기  %d = %d\n", a, b, a-b);
+	printf("%d 곱하기 %d = %d\n", a, b, a*b);
+	printf("%d 나누기 %d = %d\n", a, b, a/b);
+	printf("%d를 %d로 나눈 나머지 = %d\n", a, b, a%b); 
+}
